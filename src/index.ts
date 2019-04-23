b2c4538da39d363dfc451240267d1b86bc4ad6ad
Jean-Christophe Delmas <jean-christophe@upflow.io>
2019-04-23 20:54:28 +0200

    add index.ts and declarations

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+export { Stream, Pipe, Sink, BufferOptions } from './stream';
+export { Chunk } from './chunk';
+export { balance } from './balance';
+export { broadcast } from './broadcast';
+export { Consumer } from './consumer';
+export { lineConsumer, lines } from './text';
+export { Managed } from './managed';
